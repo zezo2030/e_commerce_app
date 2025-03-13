8bd974e208c850b8c1d6c900d9ac763026aff022
zezo2030 <zezosaad194@gmail.com>
2025-03-13 05:45:41 +0200

    create apperance

---
+++ lib/features/settings/presentation/views/appearance_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Appearance extends StatelessWidget {
+  const Appearance({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
